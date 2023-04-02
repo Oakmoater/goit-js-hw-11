168ce8fc4d2c8bcf6294775cd99fd4a3c934d5d3
Oakmoater <dubrovecva@gmail.com>
2023-04-02 22:58:36 +0200

    baseWork

--- src/index.js
+++ src/index.js
@@ -2,32 +2,45 @@ import Notiflix from 'notiflix';
 import { getImages } from '../src/js/fetchImages';
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
+const loadBtn = document.querySelector('.load-more');
+
 let pageState = 1;
 let searchState = '';
-
-console.log(getImages('cat', pageState));
 
 form.addEventListener('submit', event => {
     event.preventDefault();
     galleryParser(event.target.searchQuery.value)
 });
-const fetchImages = (search, page) => getImages(search, page)
-    .then(({hits, total}) => {
-        renderGallery(hits);
-    }).catch(e => console.log(e));
 
-function clearPage() {
-    gallery.innerHTML = '';
-    };
+loadBtn.addEventListener('click', event => {
+    event.preventDefault();
+    galleryUpload(searchState, pageState);
+});
 
 function galleryParser(searchTerm) {
     if (searchTerm !== searchState) {
-        clearPage();
         searchState = searchTerm;
-        pageState = 1;
-        fetchImages(searchTerm, pageState);
+        clearPage();
+        updateButton();
+        pageUpdate();
+        fetchImages(searchTerm, 1);
     };
 };
+
+function galleryUpload(searchState, pageState) {
+    fetchImages(searchState, pageState);
+    pageUpdate();
+};
+
+const fetchImages = (search, page) => getImages(search, page)
+    .then(({hits, total}) => {
+        renderGallery(hits);
+        if (page === 1) {
+            Notiflix.Notify.success(`Hooray! We found ${total} images.`);
+        }
+    })
+    .catch(e => console.log(e));
+
 function renderGallery(hits) {
     const galleryMarkup = hits.map(({ webformatURL, tags, likes, views, comments, downloads }) => {
         return `
@@ -52,6 +65,13 @@ function renderGallery(hits) {
     });
     gallery.insertAdjacentHTML('beforeend', galleryMarkup.join(' '));
 };
-
-
+function pageUpdate() {
+    return pageState += 1;
+}
+function clearPage() {
+    gallery.innerHTML = '';
+};
+function updateButton() {
+    loadBtn.style.display = 'block';
+};
 // largeImageURL - посилання на велике зображення.
