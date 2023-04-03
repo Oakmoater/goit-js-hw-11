0b7cee15b5ed27c72f6699327e0d80f2e3919001
Oakmoater <dubrovecva@gmail.com>
2023-04-03 22:58:29 +0200

    update visualisations

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
 import { getImages } from '../src/js/fetchImages';
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const loadBtn = document.querySelector('.load-more');
-
 let pageState = 1;
 let searchState = '';
+
+let Lightbox = new SimpleLightbox('.gallery a', {
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 
 form.addEventListener('submit', event => {
     event.preventDefault();
@@ -21,55 +28,64 @@ function galleryParser(searchTerm) {
     if (searchTerm !== searchState) {
         searchState = searchTerm;
         clearPage();
-        updateButton();
-        pageUpdate();
+        pageNumberUpdate();
         fetchImages(searchTerm, 1);
     };
 };
 
 function galleryUpload(searchState, pageState) {
     fetchImages(searchState, pageState);
-    pageUpdate();
+    pageNumberUpdate();
 };
 
 const fetchImages = (search, page) => getImages(search, page)
-    .then(({hits, total}) => {
+    .then(({ hits, total }) => {
+        if (total === 0) {
+            Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+            return
+        };
         renderGallery(hits);
+        Lightbox.refresh();
+        updateButton();
         if (page === 1) {
             Notiflix.Notify.success(`Hooray! We found ${total} images.`);
-        }
+        };
     })
     .catch(e => console.log(e));
 
 function renderGallery(hits) {
-    const galleryMarkup = hits.map(({ webformatURL, tags, likes, views, comments, downloads }) => {
+    const galleryMarkup = hits.map(({largeImageURL, webformatURL, tags, likes, views, comments, downloads }) => {
         return `
-        <div class="photo-card">
-            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-            <div class="info">
-                <p class="info-item">
-                    <b>Likes</b> ${likes}
-                </p>
-                <p class="info-item">
-                    <b>Views</b> ${views}
-                </p>
-                <p class="info-item">
-                    <b>Comments</b> ${comments}
-                </p>
-                <p class="info-item">
-                    <b>Downloads</b> ${downloads}
-                </p>
+            <div class="photo-card">
+                <a class="link__photo-card" href="${largeImageURL}" >
+                    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+                </a>
+                <div class="info">
+                    <p class="info-item">
+                        <b>Likes</b> ${likes}
+                    </p>
+                    <p class="info-item">
+                        <b>Views</b> ${views}
+                    </p>
+                    <p class="info-item">
+                        <b>Comments</b> ${comments}
+                    </p>
+                    <p class="info-item">
+                        <b>Downloads</b> ${downloads}
+                    </p>
+                </div>
             </div>
-        </div>
         `
     });
     gallery.insertAdjacentHTML('beforeend', galleryMarkup.join(' '));
 };
-function pageUpdate() {
+
+function pageNumberUpdate() {
     return pageState += 1;
 }
 function clearPage() {
     gallery.innerHTML = '';
+    Lightbox.refresh();
 };
 function updateButton() {
     loadBtn.style.display = 'block';
